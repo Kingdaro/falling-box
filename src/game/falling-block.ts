2f6e47e8655b18059569eaf114adf6b3efae40f7
just nobody <kingdaro@gmail.com>
2017-07-12 17:53:24 -0400

    limit falling block speed

--- src/game/falling-block.ts
+++ src/game/falling-block.ts
@@ -1,8 +1,15 @@
 // import * as pixi from 'pixi.js'
 import { GameObject } from './game-object'
 
-export const gravity = 1200
+const gravity = 1200
+const maxVelocity = 1000
 
 export class FallingBlock extends GameObject {
   gravity = gravity
+
+  update(dt: number) {
+    this.applyGravity(dt)
+    this.yvel = Math.min(this.yvel, maxVelocity)
+    this.applyVelocity(dt)
+  }
 }
