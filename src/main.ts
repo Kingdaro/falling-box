cd0be944381cf962e5f2dd293947dc4b0b7b4337
just nobody <kingdaro@gmail.com>
2017-07-12 01:41:00 -0400

    added a simple little silly

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
+import * as pixi from 'pixi.js'
+
 declare const require: Function
 declare const module: any
 
+class Game {
+  text = this.app.stage.addChild(new pixi.Text('hi', { fill: 'white' }))
+
+  constructor(private app: pixi.Application) {
+    this.text.position.set(100, 100)
+    this.text.anchor.set(0.5, 0.5)
+  }
+
+  update(dt: number) {
+    this.text.rotation += dt * 3
+  }
+}
+
 function run() {
-  const game = require('./game')
-  game.run()
+  const app = new pixi.Application(1280, 720)
+  const game = new Game(app)
+
+  app.ticker.add(dt => game.update(dt / 60))
+
+  document.body.innerHTML = ''
+  document.body.appendChild(app.view)
 }
 
 run()
-if (module.hot) {
-  module.hot.accept('./game', run)
-}
+if (module.hot) module.hot.accept(run)
