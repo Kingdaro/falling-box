ddecf16553dc9b7196c21adf70c3256ee79b5c77
just nobody <kingdaro@gmail.com>
2017-07-12 02:16:06 -0400

    IT MOVES

--- src/gameplay.ts
+++ src/gameplay.ts
@@ -1,7 +1,25 @@
+import * as pixi from 'pixi.js'
 import { GameState } from './game'
 
+class Player {
+  sprite = new pixi.Graphics()
+
+  constructor() {
+    this.sprite.beginFill(0xffffff)
+    this.sprite.drawRect(0, 0, 50, 50)
+    this.sprite.endFill()
+  }
+}
+
 export class GameplayState extends GameState {
+  player = new Player()
+
   enter() {
-    console.log('have entered gameplay')
+    this.stage.addChild(this.player.sprite)
+    this.player.sprite.position.set(100, 100)
+  }
+
+  update(dt: number) {
+    this.player.sprite.x += 100 * dt
   }
 }
