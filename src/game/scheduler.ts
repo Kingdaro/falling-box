60551fe4025f6cfa3f5d25ac11129eb6e3f3d802
just nobody <kingdaro@gmail.com>
2017-07-22 09:29:28 -0400

    added scheduler for easier timing of stuff

---
+++ src/game/scheduler.ts
@@ -0,0 +1,27 @@
+export class Scheduler {
+  private tasks = [] as Task[]
+
+  addTask(task: Task) {
+    this.tasks.push(task)
+  }
+
+  update(dt: number) {
+    this.tasks.forEach(task => {
+      task.time -= dt
+      if (task.time <= 0) {
+        task.action()
+        task.time += task.period
+      }
+    })
+    this.tasks = this.tasks.filter(task => task.time > 0 || task.repeat)
+  }
+}
+
+export class Task {
+  time = this.period
+  constructor(
+    public period: number,
+    public repeat: boolean,
+    public action: () => any,
+  ) {}
+}
