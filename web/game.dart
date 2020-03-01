3d559a91f82cf9c5ef96f54703162ca1f472cac4
kingdaro <kingdaro@gmail.com>
2020-02-29 18:42:12 -0600

    remove GameState class (for now)

--- web/game.dart
+++ web/game.dart
@@ -1,11 +1,6 @@
 import 'game_event.dart';
 import 'player.dart';
 
-class GameState {
-  num playerX = 0;
-  num playerY = 0;
-}
-
 class Game implements GameEventHandler {
   final _player = Player();
 
