41a25052571c295efd1ea0853869c8fed6b19dfd
kingdaro <kingdaro@gmail.com>
2020-02-29 22:54:38 -0600

    gravity

--- web/player.dart
+++ web/player.dart
@@ -6,9 +6,11 @@ import 'vector.dart';
 const playerSpeed = 500;
 const playerSize = 50;
 const playerMovementStiffness = 10;
+const playerGravity = 800;
 
 class Player implements GameEventHandler {
   var position = vec(50, 50);
+  num fallingVelocity = 0;
   final PlayerInput _input;
 
   Player(this._input);
@@ -18,7 +20,12 @@ class Player implements GameEventHandler {
     _input.handleGameEvent(event);
 
     if (event is UpdateEvent) {
+      // movement
       position += vec(_input.currentMovement * playerSpeed * event.delta, 0);
+
+      // apply gravity
+      fallingVelocity += playerGravity * event.delta;
+      position += vec(0, fallingVelocity * event.delta);
     }
 
     if (event is DrawEvent) {
