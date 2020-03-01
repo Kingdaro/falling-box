0af1d812d55736ae4560985d157281b4f79292a6
kingdaro <kingdaro@gmail.com>
2020-03-01 16:05:33 -0600

    jumping

--- web/player.dart
+++ web/player.dart
@@ -5,12 +5,14 @@ import 'input.dart';
 import 'math.dart';
 import 'vector.dart';
 
-const playerSpeed = 500;
-const playerSize = 50;
-const playerMovementStiffness = 10;
-const playerGravity = 1200;
+enum JumpState { falling, jumping }
 
 class Player implements GameEventHandler {
+  static const speed = 500;
+  static const size = 50;
+  static const gravity = 2500;
+  static const jumpSpeed = 800;
+
   var position = vec(50, 50);
   num fallingVelocity = 0;
   final _input = PlayerInput();
@@ -20,37 +22,56 @@ class Player implements GameEventHandler {
     _input.handleGameEvent(event);
 
     if (event is UpdateEvent) {
-      // movement
-      position += vec(_input.currentMovement * playerSpeed * event.delta, 0);
+      final movementDelta = _input.currentMovement * speed * event.delta;
 
-      // apply gravity
-      fallingVelocity += playerGravity * event.delta;
-      position += vec(0, fallingVelocity * event.delta);
+      if (_input.isJumping) {
+        fallingVelocity = -jumpSpeed;
+      } else {
+        fallingVelocity += gravity * event.delta;
+      }
+
+      position += vec(movementDelta, fallingVelocity * event.delta);
     }
 
     if (event is DrawEvent) {
       event.context2d
         ..fillStyle = 'white'
-        ..fillRect(position.x, position.y, playerSize, playerSize);
+        ..fillRect(position.x, position.y, size, size);
     }
   }
 }
 
 class PlayerInput implements GameEventHandler {
+  static const movementStiffness = 10;
+
   final _movementControl = Control([
     JoystickAxisInput(),
+    CombinedAxisInput(
+      JoystickButtonInput(14),
+      JoystickButtonInput(15),
+    ),
     CombinedAxisInput(KeyInput(KeyCode.LEFT), KeyInput(KeyCode.RIGHT)),
   ]);
 
   num currentMovement = 0;
 
+  final _jumpControl = Control(
+      [JoystickButtonInput(0), JoystickButtonInput(1), KeyInput(KeyCode.Z)]);
+
+  var _jumpState = JumpState.falling;
+  bool get isJumping => _jumpState == JumpState.jumping;
+
   @override
   void handleGameEvent(GameEvent event) {
     _movementControl.handleGameEvent(event);
+    _jumpControl.handleGameEvent(event);
 
     if (event is UpdateEvent) {
       currentMovement = lerp(currentMovement, _movementControl.value,
-          event.delta * playerMovementStiffness);
+          event.delta * movementStiffness);
+
+      _jumpState =
+          _jumpControl.isActive ? JumpState.jumping : JumpState.falling;
     }
   }
 }
