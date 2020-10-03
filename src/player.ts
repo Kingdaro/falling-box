67d31d6c36c7f3776f961e089105aacbe3d4d565
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-10-03 13:29:06 -0500

    limited jumping

--- src/player.ts
+++ src/player.ts
@@ -7,9 +7,11 @@ import { Rect } from "./rect"
 const speed = 500
 const gravity = 2500
 const jumpSpeed = 800
+const maxJumps = 2
 
 export class Player {
   rect = new Rect(0, -300, 40)
+  jumps = maxJumps
   xvel = 0
   yvel = 0
 
@@ -17,12 +19,13 @@ export class Player {
     let xvel = 0
     if (["ArrowLeft", "KeyA"].some(isDown)) xvel -= speed
     if (["ArrowRight", "KeyR"].some(isDown)) xvel += speed
-    this.xvel = xvel
 
-    if (wasPressed("ArrowUp")) {
+    this.xvel = xvel
+    this.yvel += gravity * dt
+
+    if (wasPressed("ArrowUp") && this.jumps > 0) {
       this.yvel = -jumpSpeed
-    } else {
-      this.yvel += gravity * dt
+      this.jumps -= 1
     }
 
     const [finalX, finalY, collisions] = collider.move(
@@ -35,10 +38,18 @@ export class Player {
     )
     this.rect.setTopLeft(finalX, finalY)
 
-    // prettier-ignore
-    for (const col of collisions) {
-      if (col.normal.x !== 0 && Math.sign(col.normal.x) !== Math.sign(this.xvel)) this.xvel = 0
-      if (col.normal.y !== 0 && Math.sign(col.normal.y) !== Math.sign(this.yvel)) this.yvel = 0
+    for (const { normal } of collisions) {
+      if (normal.x !== 0 && Math.sign(normal.x) !== Math.sign(this.xvel)) {
+        this.xvel = 0
+      }
+
+      if (normal.y !== 0 && Math.sign(normal.y) !== Math.sign(this.yvel)) {
+        this.yvel = 0
+      }
+
+      if (normal.y < 0) {
+        this.jumps = maxJumps
+      }
     }
   }
 
