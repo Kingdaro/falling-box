95c23254810751e6f934e1bfa32d9e0fe2e40aec
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-10-10 17:58:12 -0500

    new collision functions

--- src/collision.ts
+++ src/collision.ts
@@ -1,52 +1,112 @@
-import { Body, MoveFilter, ResponseType, World } from "bopit"
-import { raise } from "./helpers"
+import { Rect } from "./rect"
+import { vec, Vector } from "./vector"
 
-type ColliderFilterFn = (
-  obj: object,
-  ...args: Parameters<MoveFilter>
-) => ResponseType | undefined
+// https://www.gamedev.net/articles/programming/general-and-gameplay-programming/swept-aabb-collision-detection-and-response-r3084
 
-// https://github.com/tanema/bopit#usage
-export class Collider {
-  private readonly world
-  private readonly bodiesByObject = new WeakMap<object, Body>()
-  private readonly objectsByBody = new WeakMap<Body, object>()
+type Collision = {
+  normal: Vector
+  distance: number
+  movementDelta: Vector
+}
 
-  constructor(cellSize?: number) {
-    this.world = new World(cellSize)
+type Resolution = {
+  finalPosition: Vector
+  normal: Vector
+}
+
+export function getCollision(
+  movingRect: Rect,
+  targetRect: Rect,
+  targetPosition: Vector,
+): Collision | undefined {
+  const movementDelta = targetPosition.minus(movingRect.position)
+
+  const xInvEntry =
+    movementDelta.x > 0
+      ? targetRect.left - movingRect.right
+      : targetRect.right - movingRect.left
+
+  const xInvExit =
+    movementDelta.x > 0
+      ? targetRect.right - movingRect.left
+      : targetRect.left - movingRect.right
+
+  const yInvEntry =
+    movementDelta.y > 0
+      ? targetRect.top - movingRect.bottom
+      : targetRect.bottom - movingRect.top
+
+  const yInvExit =
+    movementDelta.y > 0
+      ? targetRect.bottom - movingRect.top
+      : targetRect.top - movingRect.bottom
+
+  const xEntry = movementDelta.x === 0 ? Infinity : xInvEntry / movementDelta.x
+  const xExit = movementDelta.x === 0 ? Infinity : xInvExit / movementDelta.x
+  const yEntry = movementDelta.y === 0 ? Infinity : yInvEntry / movementDelta.y
+  const yExit = movementDelta.y === 0 ? Infinity : yInvExit / movementDelta.y
+
+  const entryTime = Math.max(xEntry, yEntry)
+  const exitTime = Math.min(xExit, yExit)
+
+  // no collision
+  if (
+    entryTime > exitTime ||
+    (xEntry < 0 && yEntry < 0) ||
+    xEntry > 1 ||
+    yEntry > 1
+  ) {
+    return undefined
   }
 
-  add(obj: object, left: number, top: number, width: number, height: number) {
-    const body = new Body(left, top, width, height)
-    this.bodiesByObject.set(obj, body)
-    this.objectsByBody.set(body, obj)
-    body.add(this.world)
-  }
-
-  // TODO: make this return an object instead of a tuple
-  move(obj: object, left: number, top: number, filter?: ColliderFilterFn) {
-    const body = this.bodiesByObject.get(obj) ?? raise("body not found")
-    return body.move(left, top, (body, ...args) => {
-      if (!filter) return "slide"
-      const obj = this.objectsByBody.get(body) ?? raise("object not found")
-      return filter(obj, body, ...args)
-    })
-  }
-
-  remove(obj: object) {
-    const body = this.bodiesByObject.get(obj)
-    if (body) {
-      this.bodiesByObject.delete(obj)
-      this.objectsByBody.delete(body)
-      body.destroy()
+  if (xEntry > yEntry) {
+    return {
+      normal: vec(Math.sign(xInvEntry), 0),
+      distance: entryTime,
+      movementDelta,
+    }
+  } else {
+    return {
+      normal: vec(0, Math.sign(yInvEntry)),
+      distance: entryTime,
+      movementDelta,
     }
   }
+}
 
-  setPosition(obj: object, left: number, top: number) {
-    const body = this.bodiesByObject.get(obj)
-    if (body) {
-      this.remove(obj)
-      this.add(obj, left, top, body.w, body.h)
-    }
+export function resolveTouch(
+  movingRect: Rect,
+  targetRect: Rect,
+  targetPosition: Vector,
+): Resolution | undefined {
+  const collision = getCollision(movingRect, targetRect, targetPosition)
+  if (!collision) return
+
+  return {
+    normal: collision.normal,
+    finalPosition: movingRect.position.plus(
+      collision.movementDelta.times(collision.distance),
+    ),
+  }
+}
+
+export function resolveSlide(
+  movingRect: Rect,
+  targetRect: Rect,
+  targetPosition: Vector,
+): Resolution | undefined {
+  const collision = getCollision(movingRect, targetRect, targetPosition)
+  if (!collision) return
+
+  const remainingDistance = 1 - collision.distance
+  const dotProduct = collision.movementDelta.dotProduct(collision.normal)
+
+  const finalPosition = movingRect.position
+    .plus(collision.movementDelta.times(collision.distance))
+    .plus(collision.normal.times(dotProduct * remainingDistance))
+
+  return {
+    normal: collision.normal,
+    finalPosition,
   }
 }
